4741ee0522c2aba45835a200d92eaf0315c381de
o-sam-o <sam.cavenagh@gmail.com>
2013-08-01 19:19:45 -0700

    remove need to pass $scope to getPhoto

--- beer.js
+++ beer.js
@@ -7,9 +7,9 @@
   services.factory('photoService', function($resource) {
     var Photo = $resource('http://api.flickr.com/services/rest/', 
                           {
-                             api_key: API_KEY,
-                             format: 'json',
-                             nojsoncallback: 1
+                            api_key: API_KEY,
+                            format: 'json',
+                            nojsoncallback: 1
                           },
                           {
                             get: {
@@ -31,24 +31,18 @@
                             }
                           });
 
-    return {
-      getPhotos: function($scope) {
-        return Photo.query({photoset_id: PHOTOSET_ID});
-      }
-    };
+                          return {
+                            getPhotos: function() {
+                              return Photo.query({photoset_id: PHOTOSET_ID});
+                            }
+                          };
   });
 
   var beers = angular.module('beers', ['beerServices']);
 
   beers.controller('PhotoListCtrl', 
-    function PhotoListCtrl($scope, photoService) {
-      $scope.photos = photoService.getPhotos();
-      //$scope.photos = [
-        //{"title":"Lao Brewery Co - Beerlao","src":"http://farm4.staticflickr.com/3689/9225697519_008d6e5a83_m.jpg"},
-        //{"title":"Brouwerij Timmermans John Martin N.V. - Bourgogne Des Flandres","src":"http://farm3.staticflickr.com/2870/9225698103_6c7a7fed3c_m.jpg"},
-        //{"title":"Stieglbrauerei zu Salzburg - Stiegl Weisse","src":"http://farm3.staticflickr.com/2874/9228479838_4abf43e4b8_m.jpg"},
-        //{"title":"Weihenstephaner - Hefeweissbier Dark","src":"http://farm4.staticflickr.com/3741/9225699267_6c329efbcc_m.jpg"}
-      //];
-    }
-  );
+                   function PhotoListCtrl($scope, photoService) {
+                     $scope.photos = photoService.getPhotos();
+                   }
+                  );
 }());
