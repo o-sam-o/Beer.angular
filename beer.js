41161ccd1a95627c28d1a1fa264b88d02c914d2a
o-sam-o <sam.cavenagh@gmail.com>
2013-08-22 21:53:39 -0700

    Add support for saving photo summary in local storage

--- beer.js
+++ beer.js
@@ -19,8 +19,20 @@
       return this.attr.url_s;
     },
 
+    getSmallWidth: function() {
+      return this.attr.width_s;
+    },
+
+    getSmallHeight: function() {
+      return this.attr.height_s;
+    },
+
     compareByTitle: function(other) {
       return (this.getTitle() < other.getTitle() ? -1 : (this.getTitle() > other.getTitle() ? 1 : 0));
+    },
+
+    toHash: function() {
+      return this.attr;
     }
   }
 
@@ -40,6 +52,97 @@
     }
   };
 
+  var LsLinkList = function() {
+  }
+  LsLinkList.prototype = new Array;
+
+  var LocalStorageDB = function() {
+
+  }
+
+  LocalStorageDB.prototype = {
+
+    load: function(key, noEntryCallback) {
+      var storableMeta = this._get(key);
+
+      if(storableMeta) {
+        return this._makeArrayLike(storableMeta);
+      }
+
+      var array = noEntryCallback();
+      if (array.$then) {
+        //Handle promise
+        array.$then(function(response) {
+          this._storeArray(key, response.data);
+          return response;
+        }.bind(this));
+      } else {
+        this._storeArray(key, array);
+      }
+      return array;
+    },
+
+    _getEntryKey: function(key, entry) {
+      return '_ll_' + key + '_' + entry.id;
+    },
+
+    _storeArray: function(key, array) {
+      var previous;
+      array.map(function(entry) {
+        if(previous) {
+          previous._llNextKey = this._getEntryKey(key, entry);
+        }
+        previous = entry;
+        return entry;
+      }, this).forEach(function(entry) {
+        localStorage.setItem(this._getEntryKey(key, entry), JSON.stringify(entry));
+      }, this);
+
+      localStorage.setItem(key, JSON.stringify({
+        length: array.length,
+        firstKey: (array[0] ? this._getEntryKey(key, array[0]) : null)
+      }));
+    },
+
+    _makeArrayLike: function(storableMeta) {
+      var first = this._get(storableMeta.firstKey);
+      var retrievedEntries = {
+        0: first
+      }
+
+      var arrayLike = new LsLinkList
+      arrayLike[0] = first;
+      arrayLike.length = storableMeta.length;
+
+      var lsGet = this._get;
+      for(var i = 1; i < storableMeta.length; i++) {
+        (function(index) {
+          Object.defineProperty(arrayLike, index, {
+            get: function() {
+              if(!first || index < 0) {
+                return null;
+              }
+              if(!retrievedEntries[index]) {
+                var previous = arrayLike[index - 1];
+                retrievedEntries[index] = lsGet(previous._llNextKey);
+              }
+
+              return retrievedEntries[index];
+            }
+        });
+        })(i);
+      }
+
+      return arrayLike;
+    },
+
+    _get: function(key) {
+      return JSON.parse(localStorage.getItem(key));
+    }
+  }
+
+  var lsDb = new LocalStorageDB();
+
   var services = angular.module('beerServices', ['ngResource']);
   services.factory('photoService', function($resource) {
     var Photo = $resource('http://api.flickr.com/services/rest/', 
@@ -55,7 +158,7 @@
                                 method: 'flickr.photos.getInfo'
                               },
                               transformResponse: function(data) {
-                                return new PhotoModel(JSON.parse(data).photo);
+                                return JSON.parse(data).photo;
                               }
                             },
                             query: {
@@ -66,19 +169,18 @@
                               },
                               isArray: true,
                               transformResponse: function(data) {
-                                return JSON.parse(data).photoset.photo.map(function(attr) {
-                                  return new PhotoSummary(attr);
-                                });
-                              }
+                                return JSON.parse(data).photoset.photo;                              }
                             }
                           });
 
                           return {
                             getPhotos: function() {
-                              return Photo.query({photoset_id: PHOTOSET_ID});
+                              return lsDb.load('photoStream', function() {
+                                return Photo.query({photoset_id: PHOTOSET_ID});
+                              })
                             },
                             getPhoto: function(id) {
-                              return Photo.get({photo_id: id});
+                              return new PhotoModel(Photo.get({photo_id: id}));
                             }
                           };
   });
@@ -103,6 +205,19 @@
                    function PhotoListCtrl($scope, photoService) {
                      $scope.sortOrder = 'alpha'
                      $scope.photos = photoService.getPhotos();
+
+                     //TODO refactor this as it's super ugly
+                     if($scope.photos.$then) {
+                       $scope.photos.$then(function(response) {
+                         $scope.photos = response.data.map(function(photo) {
+                           return new PhotoSummary(photo);
+                         });
+                       });
+                     } else {
+                       $scope.photos = $scope.photos.map(function(photo) {
+                         return new PhotoSummary(photo);
+                       });
+                     }
                    }
                   );
 
